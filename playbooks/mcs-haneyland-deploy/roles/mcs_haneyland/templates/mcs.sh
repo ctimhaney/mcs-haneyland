0af773c8b00501eea27bd039c3400a48f2cf6751
Colin Haney <ctimhaney@gmail.com>
2022-01-15 14:07:51 -0600

    Updating server image to 1.18.1

--- playbooks/mcs-haneyland-deploy/roles/mcs_haneyland/templates/mcs.sh
+++ playbooks/mcs-haneyland-deploy/roles/mcs_haneyland/templates/mcs.sh
@@ -4,21 +4,21 @@ VOLUME_NAME="{{ docker_volume_name }}"
 DOCKER_IMAGE_TAG="{{ docker_image_tag }}"
 SCRIPT_DIR=$(dirname $(realpath $0))
 
-if [ $UID -ne 0 ]
+if [ ${UID} -ne 0 ]
 then
   echo "Communicating with the docker daemon requires privilege escalation"
   exit 1
 fi
 
-test -f $SCRIPT_DIR/environment.sh
+test -f ${SCRIPT_DIR}/environment.sh
 
 if [ $? -eq 0 ]
 then
-  source $SCRIPT_DIR/environment.sh
+  source ${SCRIPT_DIR}/environment.sh
 fi
 
 status() {
-  docker container inspect $CONTAINER_NAME
+  docker container inspect ${CONTAINER_NAME}
 }
 
 start() {
@@ -29,9 +29,9 @@ start() {
     --rm \
     --publish 25565:25565 \
     --env JAVA_OPTIONS='-Xms{{ java_opts_Xms }} -Xmx{{ java_opts_Xmx }}' \
-    --name $CONTAINER_NAME \
-    --mount source=$VOLUME_NAME,target=/opt/mcs-haneyland \
-    colinthaney/mcs-haneyland:latest
+    --name ${CONTAINER_NAME} \
+    --mount source=${VOLUME_NAME},target=/opt/mcs-haneyland \
+    ${DOCKER_IMAGE_TAG}
   else
     echo "$CONTAINER_NAME already started"
   fi
@@ -41,9 +41,9 @@ stop() {
   status > /dev/null 2>&1
   if [ $? -eq 0 ]
   then
-    docker container stop $CONTAINER_NAME
+    docker container stop ${CONTAINER_NAME}
   else
-    echo "$CONTAINER_NAME not running"
+    echo "${CONTAINER_NAME} not running"
   fi
 }
 
@@ -56,16 +56,16 @@ restart() {
   start
 }
 
-if [ "$1" == "status" ]
+if [ "${1}" == "status" ]
 then
   status
-elif [ "$1" == "start" ]
+elif [ "${1}" == "start" ]
 then
   start
-elif [ "$1" == "stop" ]
+elif [ "${1}" == "stop" ]
 then
   stop
-elif [ "$1" == "restart" ]
+elif [ "${1}" == "restart" ]
 then
   restart
 else
